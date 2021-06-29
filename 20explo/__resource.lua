02bc055755feaa5c1bea5f49f20416cf07667d3f
twit4444 <76741222+twit4444@users.noreply.github.com>
2021-06-28 21:38:54 -0400

    Add files via upload

---
+++ 20explo/__resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+-- Template made by McGinlay
+-- Please consider subscribing!
+
+files {
+	'vehicles.meta',
+	'carcols.meta',
+	'handling.meta',
+	'carvariations.meta'
+	
+}
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
